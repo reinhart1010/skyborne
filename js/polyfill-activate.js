bbb2a64dab27ae8b1bb17aa2f6eb2edec454f909
Reinhart Previano Koentjoro <reinhart@reinhart1010.id>
2022-01-31 00:39:00 +0700

    fix: Polyfill loading

--- js/polyfill-activate.js
+++ js/polyfill-activate.js
@@ -1,5 +1,8 @@
 window.onload = function(){
   if (typeof CSS === 'undefined' || !CSS.supports('color', 'var(--fake-var)')){
-    document.write('<script src="https://cdn.jsdelivr.net/gh/nuxodin/ie11CustomProperties@4.1.0/ie11CustomProperties.min.js"><\/script>')
+    var newScript = document.createElement('script');
+    newScript.type = 'text/javascript';
+    newScript.src = 'https://cdn.jsdelivr.net/gh/nuxodin/ie11CustomProperties@4.1.0/ie11CustomProperties.min.js';
+    document.getElementsByTagName('head')[0].appendChild(newScript);
   }
 }
